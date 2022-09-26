c0339934fd9d1b35057324a48ecb7d9548c634cc
Your Name <you@example.com>
2022-09-26 22:09:35 +0800

    views

--- app/Http/Controllers/welcomeController.php
+++ app/Http/Controllers/welcomeController.php
@@ -9,7 +9,9 @@ class welcomeController extends Controller
     public function about(){
         // return "hi";
         $name="悟空";
-        return view("page/about",['x'=>$name]);
+        $heroes=["悟空","達爾","名人","魯夫"];
+        $js = "<script>alert('hi');</script>";
+        return view("page/about",['x'=>$name,'js'=>$js,'heroes'=>$heroes]);
         // 用.也可以
         // return view("page.about");
     }
--- resources/views/page/about.blade.php
+++ resources/views/page/about.blade.php
@@ -1,2 +1,18 @@
 <h1>hi</h1>
 {{$x}}
+{!! $js !!}
+
+@if(count($heroes)>0)
+    @foreach ($heroes as $hero)
+        @if($loop->first || $loop->last)
+    <li> 
+        <strong>{{$hero}}</strong>
+    </li>
+    @else
+    <li>
+        {{$hero}}
+    </li>
+    
+    @endif
+@endforeach
+@endif
